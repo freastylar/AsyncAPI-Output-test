b32816b06748833590c06955c2a32545b7ede332
Leander.Bunte <bunte.l@icloud.com>
2025-06-28 18:59:31 +0200

    opentelemtry-jaeger error cant be resolved

--- src/tracing/mod.rs
+++ src/tracing/mod.rs
@@ -4,7 +4,7 @@ use opentelemetry::sdk::propagation::TraceContextPropagator;
 pub fn init_jaeger_tracer(service_name: &str) {
     global::set_text_map_propagator(TraceContextPropagator::new());
 
-    opentelemetry_jaeger::new_agent_pipeline()
+    opentelemetry_jaeger::new_pipeline()
         .with_service_name(service_name)
         .install_batch(opentelemetry::runtime::Tokio)
         .expect("Failed to initialize Jaeger Tracer");
