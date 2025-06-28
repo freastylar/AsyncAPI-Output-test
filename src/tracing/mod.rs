e2a0cfd263de3498451f825b7e7f41b62f96aa07
Leander.Bunte <bunte.l@icloud.com>
2025-06-28 15:32:51 +0200

    newer versions of nuid use rand::distr:: instead of rand::distributions:: so it has to be forced that they use rand version 0.8.x (i chose 0.8.5), which can be changed in the cargo.lock

--- src/tracing/mod.rs
+++ src/tracing/mod.rs
@@ -1,7 +1,9 @@
 use opentelemetry::global;
+use opentelemetry::sdk::propagation::TraceContextPropagator;
 
 pub fn init_jaeger_tracer(service_name: &str) {
-    global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
     opentelemetry_jaeger::new_agent_pipeline()
         .with_service_name(service_name)
         .install_batch(opentelemetry::runtime::Tokio)
